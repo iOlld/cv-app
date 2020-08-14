27e0e8533162e0b38f1ec7d10207490d9ef60cba
Zheka-ded <11vitamin11@rambler.ru>
2020-08-14 06:44:09 +0300

    footer and button wave

--- my-cv-v-2.2/build/js/main.js
+++ my-cv-v-2.2/build/js/main.js
@@ -26,3 +26,41 @@ function spanAddStyle() {
         })
     }, 800);
 };
+
+let links = document.querySelectorAll("button");
+for(let i = 0; i < links.length; i++){
+    links[i].addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Remove any old one
+        let ripple = document.querySelector('.ripple');
+        if (ripple) {
+            ripple.remove();
+        }
+
+        // Setup
+        let buttonWidth = this.offsetWidth,
+            buttonHeight = this.offsetHeight;
+
+        // Make it round!
+        if(buttonWidth >= buttonHeight) {
+            buttonHeight = buttonWidth;
+        } else {
+            buttonWidth = buttonHeight;
+        }
+
+        // Get the center of the element
+        let x = event.offsetX==undefined?event.layerX:event.offsetX - buttonWidth / 2,
+            y = event.offsetY==undefined?event.layerY:event.offsetY - buttonHeight / 2;
+
+        // Add the element
+        let span = document.createElement('span');
+        span.className = 'ripple';
+        s = span.style;
+        s.width = buttonWidth + 'px';
+        s.height = buttonHeight + 'px';
+        s.top = y + 'px';
+        s.left = x + 'px';
+        this.appendChild(span);
+    });
+}
