f43e7e425bbe03d20e2af4406aa50ba9c4c007d5
Zheka-ded <11vitamin11@rambler.ru>
2020-08-21 07:37:44 +0300

    correct contact

--- my-cv-v-2.2/build/js/contact/contact.js
+++ my-cv-v-2.2/build/js/contact/contact.js
@@ -24,34 +24,16 @@ const CHECK_Email = function(){
 
 EMAIL.oninput = CHECK_Email
 
-// checkEmail.test(formUserEmail.value.trim())
 
-// const FUNC_CHECK = function () {
-
-//     let bank = [];
-
-//     for(let i = 0; i < FORM.elements.length; i++) {
-//         // bank.push(FORM.elements[i].value)
-//         bank[i] = FORM.elements[i].value
-//     }
-
-//     console.log(`1`);
-//     console.log(bank);
-
-//     console.log(this)
-
-// }
 const FUNC_CHECK = function () {
     if(NAME.value.trim().length >= 2 && EMAIL.value.trim() !== '' && MESSAGE.value.trim() !== ''){
         
-        console.log(`лайк блядь`);
+        console.log('+');
     } else {
-        alert('Какого хуя???')
+        alert('Заполните обязательные поля')
     }
-    // console.log(NAME.value.length)
 }
 
 SUBMIT.onclick = FUNC_CHECK;
 
 console.log(FORM.elements)
-// console.log(form["user-email"].value)
