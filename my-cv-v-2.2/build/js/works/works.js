e28102d776468cdb6091542334c0d3752a0e1fd0
Zheka-ded <11vitamin11@rambler.ru>
2020-08-23 11:35:32 +0300

    add nav link

--- my-cv-v-2.2/build/js/works/works.js
+++ my-cv-v-2.2/build/js/works/works.js
@@ -1,22 +1,5 @@
-// console.log(window.navigator.userAgent.toLowerCase())
-// console.log(window.navigator.userAgent)
+let innerFront = document.querySelectorAll('.inner-front');
 
-let a;
-if (navigator.userAgent.search(/Safari/) > 0) {a = 'Safari'};
-if (navigator.userAgent.search(/Firefox/) > 0) {a = 'MozillaFirefox'};
-if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {a = 'Internet Explorer'};
-if (navigator.userAgent.search(/Chrome/) > 0) {a = 'Google Chrome'};
-if (navigator.userAgent.search(/YaBrowser/) > 0) {a = 'Яндекс браузер'};
-if (navigator.userAgent.search(/OPR/) > 0) {a = 'Opera'};
-if (navigator.userAgent.search(/Konqueror/) > 0) {a = 'Konqueror'};
-if (navigator.userAgent.search(/Iceweasel/) > 0) {a = 'Debian Iceweasel'};
-if (navigator.userAgent.search(/SeaMonkey/) > 0) {a = 'SeaMonkey'};
-if (navigator.userAgent.search(/Edge/) > 0) {a = 'Microsoft Edge'};
-// alert(a); // В переменной a будет название браузера
-
-
-if (/iPhone|iPad|iPod/i.test(navigator.userAgent) && a === 'Safari') {
-    // alert('fucking apple')
-} else {
-    // alert('norm')
+for(let i = 0; i < innerFront.length; i++) {
+    innerFront[i].onclick = () => false;
 }
